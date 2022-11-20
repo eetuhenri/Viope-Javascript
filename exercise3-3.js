a0b5ec94f5d9a266e5fc787c747771d3fbd126c1
eetuhenri <118538158+eetuhenri@users.noreply.github.com>
2022-11-20 17:20:49 +0200

    Create exercise3-3.js

---
+++ exercise3-3.js
@@ -0,0 +1,6 @@
+
+var nappula = document.createElement("button");
+nappula.id = "luotuPainike";
+nappula.innerHTML = "";	
+nappula.setAttribute("onclick", "tervehdi();");
+document.body.appendChild(nappula);
