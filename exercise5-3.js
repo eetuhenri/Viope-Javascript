66900faacf96160c90294ba264bc2f2dc0a2d419
eetuhenri <118538158+eetuhenri@users.noreply.github.com>
2022-11-20 17:22:59 +0200

    Create exercise5-3.js

---
+++ exercise5-3.js
@@ -0,0 +1,14 @@
+console.log("Taulukko 1: " + array1)
+console.log("Taulukko 2: " + array2)
+
+let numero = 1;	
+	
+while (array2.length > 0) {
+if(numero%2==1){
+	numero++
+array1.push(array2.pop());
+}else if (numero%2==0){
+array1.push(array2.shift());
+	numero++
+}}
+console.log("Lopputulos: " + array1)
