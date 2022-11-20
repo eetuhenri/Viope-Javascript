1f78f986c37009b9e50f49b11b8e1a4b30dad020
eetuhenri <118538158+eetuhenri@users.noreply.github.com>
2022-11-20 17:22:16 +0200

    Create exercise5-1.js

---
+++ exercise5-1.js
@@ -0,0 +1,10 @@
+var kimmo =
+	{nimi: function() {return "Kimmo Koodari"},
+	etunimi: "Kimmo", 
+	ika: 20 
+	}
+var oona =
+	{nimi: function() {return "Oona Ohjelmoija"},
+	 ika: 32,
+	 etunimi: "Oona"
+	}
