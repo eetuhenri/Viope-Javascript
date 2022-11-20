5a1649a112e1f29adf654bc826841293a2df2702
eetuhenri <118538158+eetuhenri@users.noreply.github.com>
2022-11-20 17:17:55 +0200

    Create exercise1-3.js

---
+++ exercise1-3.js
@@ -0,0 +1,14 @@
+var eka = 17
+var toka = 3
+var summa = (eka + toka)
+var erotus = (eka - toka)
+var tulo = (eka * toka)
+var osamaara = (eka / toka)
+var jakojaannos = ( eka % toka)
+
+console.log("Luvut ovat "+eka +" ja " + toka)
+console.log("Summa on "+ summa)
+console.log("Erotus on "+ erotus)
+console.log("Tulo on "+ tulo)
+console.log("Osamäärä on "+ osamaara)
+console.log("Jakojäännös on "+ jakojaannos)
